c4515c5217e02a97c1f246524fcc9f225f29b801
R-Morton <remann12@gmail.com>
2023-03-16 14:04:45 +1100

    JWT Authorization in place

--- src/controller/users_controller.py
+++ src/controller/users_controller.py
@@ -1,46 +1,46 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 import functools
 from schema.users_schema import user_schema, users_schema
 from schema.roles_schema import role_schema
 from model.user import User, Role
 from model.venue import Venue
 from main import db, login_manager
-from flask_login import LoginManager, login_user, login_required, logout_user
-
+from flask_jwt_extended import get_jwt_identity, create_access_token, jwt_required, JWTManager
 
 user = Blueprint('user', __name__, url_prefix='/users')
 
-def make_secure(func):
-    @functools.wraps(func)
-    def secure_function(*args, **kwargs):
-        user = User()
-        if user.role_id >= 2:
-            return func(*args, **kwargs)
-        else:
+def make_secure(*access_level):
+    def decorator(func):
+        @functools.wraps(func)
+        def secure_function(*args, **kwargs):
+            user_id = get_jwt_identity()
+            user = User.query.get(user_id)
+            for level in access_level:
+                if user.roles.name == level:
+                    return func(*args, **kwargs)
             return {"message": "You are not authorized"}
+        return secure_function
+    return decorator
 
-@login_manager.request_loader
-def load_user_from_request(request):
-    api_key = request.args.get('api_key')
-    if api_key:
-        user = User.query.filter_by(api_key=api_key).first()
-        if user:
-            return user
+def admin_only():
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+    if user.roles.name == "Admin":
+        return True
+    else:
+        return False
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(user_id)
-
-@login_manager.unauthorized_handler
-def unauthorized():
-    return {"message": "you must be logged in"}
 
 @user.get("/")
+@jwt_required()
+@make_secure("Admin","Manager")
 def get_users():
     users = User.query.all()
     return users_schema.dump(users)
 
 @user.get("/<int:id>")
+@jwt_required()
+@make_secure("Admin","Manager")
 def get_user(id):
     user = User.query.get(id)
 
@@ -49,11 +49,17 @@ def get_user(id):
     return user_schema.dump(user)
 
 @user.post("/register")
+@jwt_required()
+@make_secure("Admin","Manager")
 def register_user():
     user_fields = user_schema.load(request.json)
     user = User(** user_fields)
     email = user.email
 
+    if user.role_id == 1:
+        if admin_only() == False:
+            return {"message": "Only admins can create an admin account"}
+        
     if '@' not in user.email:
         return {"message": "Please enter a valid email"}
     
@@ -68,7 +74,7 @@ def register_user():
 
         return user_schema.dump(user)
 
-@user.route('/login')
+@user.post('/login')
 def user_login():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
@@ -77,16 +83,16 @@ def user_login():
     if not user:
         return {"message": "user not found"}
     else:
-        login_user(user)
-        return {"message": "you are logged in"}
+        access_token = create_access_token(identity=str(user.id))
+        return jsonify(access_token)
 
 @user.route('/logout')
-@login_required
 def user_logout():
-    logout_user()
     return {"message": "You are now logged out"}
 
 @user.delete('/delete/<int:id>')
+@jwt_required()
+@make_secure("Admin","Manager")
 def user_delete(id):
     user = User.query.filter_by(id=id).first()
     if not user:
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_login import LoginManager, UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager
 
 
 db = SQLAlchemy()
@@ -11,12 +12,15 @@ login_manager = LoginManager()
 def create_app():
     app = Flask(__name__)
     login_manager.init_app(app)
+    jwt = JWTManager(app)
 
     app.config.from_object("config.app_config")
     app.config['SECRET_KEY'] = 'supersecretbruh'
+    app.config["JWT_SECRET_KEY"] = "Backend best end"
 
     db.init_app(app)
     ma.init_app(app)
+    jwt = JWTManager(app)
 
     from command.db import db_cmd
     app.register_blueprint(db_cmd)
